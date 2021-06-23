d3b32513f3924b43c72a8029e297a6f629def48f
Jakub Partyka <jakubpart@gmail.com>
2021-06-23 10:50:27 +0200

    POST support added, during handling request get also body request

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from datetime import datetime
 from flask import Flask, request, render_template
 import sys
+
 app = Flask(__name__)
 
-@app.route('/')
-def handle_request():
 
+@app.route('/', methods=['GET', 'POST'])
+def handle_request():
     # Time when the server got the request
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
 
     # Get the content of the request
     request_info = '\n--------------------'
-    request_info += '\nI got a request!'
+    request_info += '\nI just got a request!'
     request_info += '\n%s' % current_time
     request_info += '\nRequest:\n%s' % request
     request_info += '\nArgs:\n%s' % request.args
+    request_info += '\nBody:\n%s' % request.data
     request_info += '\n--------------------\n'
 
     # Print to terminal
